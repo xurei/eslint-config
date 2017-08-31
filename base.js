7000b763ed0a1c40035f2a87eb674e467868700e
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2017-08-31 16:25:38 +0200

    Fixed eslint issues

--- base.js
+++ base.js
@@ -1,47 +1,47 @@
 module.exports = {
-	"extends": "eslint:recommended",
+	'extends': 'eslint:recommended',
 	
-	"env": {
-		"commonjs": true,
-		"node": true
+	'env': {
+		'commonjs': true,
+		'node': true
 	},
 	
-	"plugins": [
-		"promise",
-		"security",
+	'plugins': [
+		'promise',
+		'security',
 	],
 	
-	"parserOptions": {
-		"ecmaVersion": 6,
-		"sourceType": "module",
+	'parserOptions': {
+		'ecmaVersion': 6,
+		'sourceType': 'module',
 	},
 	
-	"rules": {
-		"eol-last": "error",
-		"no-alert": "warn",
-		"no-eval": "error",
-		"no-const-assign": "error",
-		"semi": "warn",
-		"prefer-const": "warn",
-		"no-unused-vars": "warn",
-		"strict": "error",
-		"eqeqeq": "warn",
-		"curly": "warn",
-		"complexity": ["error", 10],
+	'rules': {
+		'eol-last': 'error',
+		'no-alert': 'warn',
+		'no-eval': 'error',
+		'no-const-assign': 'error',
+		'semi': 'warn',
+		'prefer-const': 'warn',
+		'no-unused-vars': 'warn',
+		'strict': 'error',
+		'eqeqeq': 'warn',
+		'curly': 'warn',
+		'complexity': ['error', 10],
 		
 		//Code style rules
-		"quotes": ["warn", "single"],
-		"indent": ["warn", "tab", { "SwitchCase": 1 }],
-		"prefer-template": "warn",
-		"key-spacing": ["warn", { "beforeColon": false, "afterColon": true }],
+		'quotes': ['warn', 'single'],
+		'indent': ['warn', 'tab', { 'SwitchCase': 1 }],
+		'prefer-template': 'warn',
+		'key-spacing': ['warn', { 'beforeColon': false, 'afterColon': true }],
 		
-		"promise/always-return": "error",
-		"promise/no-return-wrap": "error",
-		"promise/param-names": "error",
-		"promise/catch-or-return": "error",
-		"promise/no-native": "off",
-		"promise/no-nesting": "warn",
-		"promise/no-promise-in-callback": "warn",
-		"promise/no-callback-in-promise": "warn",
+		'promise/always-return': 'error',
+		'promise/no-return-wrap': 'error',
+		'promise/param-names': 'error',
+		'promise/catch-or-return': 'error',
+		'promise/no-native': 'off',
+		'promise/no-nesting': 'warn',
+		'promise/no-promise-in-callback': 'warn',
+		'promise/no-callback-in-promise': 'warn',
 	}
 };
